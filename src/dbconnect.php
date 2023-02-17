de71c9b68047b57584cb034f6c174cb174d3b1a3
Makoto Jimbo <makotoda4649@gmail.com>
2023-02-17 15:07:18 +0900

    uhu

---
+++ src/dbconnect.php
@@ -0,0 +1,15 @@
+
+
+<?php
+/* ドライバ呼び出しを使用して ODBC データベースに接続する */
+$dsn = 'mysql:dbname=posse;host=db';
+$user = 'root';
+$password = 'root';
+
+$dbh = new PDO($dsn, $user, $password);
+
+
+
+//以下は通常index.phpに書かれる。
+
+
